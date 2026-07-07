023ab56135a3095a257b20cc12d0a03be5e00d57
stefaniepirhofer <stefanie.pirhofer@outlook.de>
2026-07-07 02:15:27 +0200

    created basics for different languages

---
+++ app/src/main/java/com/example/commingsoon/language/AppLanguageViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.commingsoon.language
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class AppLanguageViewModel : ViewModel() {
+    var currentLanguage by mutableStateOf(AppLanguage.ENGLISH)
+        private set
+
+    fun setLanguage(language: AppLanguage) {
+        currentLanguage = language
+    }
+}
